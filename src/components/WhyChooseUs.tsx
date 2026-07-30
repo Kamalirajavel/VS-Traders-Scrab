fce7bc0f26603a58e27fc000f6e40f17305f71af
Kamalirajavel <kamali.rajavel.692006@gmail.com>
2026-07-30 13:24:27 +0530

    Update WhyChooseUs.tsx

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -25,7 +25,7 @@ const benefits = [
 
 const WhyChooseUs = () => {
   return (
-    <section className="py-24 bg-background">
+    <section className="py-10 md:py-24 bg-background">
       <div className="container mx-auto px-4 lg:px-8">
         <div className="grid lg:grid-cols-2 gap-16 items-center">
           {/* Left */}
